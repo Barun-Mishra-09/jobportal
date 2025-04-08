ae652d9519d20d0f4c12eeecdf140619434cd38c
Barun Mishra <barunmishra0911.com>
2025-04-08 20:55:08 +0530

    url changed

--- frontend/src/components/auth/api.js
+++ frontend/src/components/auth/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1/user",
+  baseURL: "https://jobportal-3bym.onrender.com/api/v1/user",
   withCredentials: true,
 });
 
